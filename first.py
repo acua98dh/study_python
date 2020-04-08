6ae5437f165923ef9daf00d4fd53ccb1579a67ae
acua98dh <vbnsibc@gmail.com>
2020-04-08 18:18:57 +0900

    add first.py

---
+++ first.py
@@ -0,0 +1,18 @@
+# Hello World
+msg = "Hello World"
+print(msg)
+
+# in
+y = [1,2,3]
+x= 1
+
+if x in y:
+    print("x in y")
+
+# how to use not
+is_ok = True
+
+if not is_ok:
+    print("NG")
+else:
+    print("OK")
